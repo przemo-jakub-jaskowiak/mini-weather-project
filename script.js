715824497f330a0e5792af08e06ed5f579fdfa36
Przemek <57060549+przemo-jakub-jaskowiak@users.noreply.github.com>
2022-02-14 13:12:04 +0100

    HomeWork 1

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let celsius;
-let temp;
-
-const farenheit = (tu=0) => {
-    celsius = tu;
-    temp = celsius*1.8+32;
+let cel = document.querySelector('.numTemp');
+const dis = document.querySelector('.display');
+const dis2 = document.querySelector('.display2');
+const btn2 = document.querySelector('.cleaner');
 
 
-    console.log(`${celsius}°C ⛅ = ${temp.toFixed(1)}°F 🌡️`);
+
+const faren = (e) => {
+    let temp = e.target.value;
+wynik = temp*1.8+32;
+wynik2 = parseInt(temp) + 273.15;
+dis.innerText = `${wynik} °F 🌤️`;
+dis2.innerText = `${wynik2} °K ☀️`;
 }
 
-farenheit(21)
+const clean = () => {
+    dis.innerText = 'Temperature result in Fahrenheit';
+dis2.innerText = 'Temperature result in Kelvin';
+}
+
+const bbb =(e)=>{
+    const zero = e.target.textContent;
+    console.log(eval(zero + "GAZ"));
+}
+
+
+
+cel.addEventListener('keyup',faren)
+btn2.addEventListener('click',clean)
